ebbff6c959d92c6c6dff2b5da9f6dda09dc6f8bb
DiptoChakrabarty <diptochuck123@gmail.com>
2023-02-01 22:14:38 +0530

    change methods

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from mysql.conenctor import errorcode
+from mysql.connector import errorcode
 
 class MySQLConnect:
     def __init__(self, config):
@@ -26,8 +26,14 @@ class MySQLConnect:
             self.cnx.close()
 
 class MySQLOperation:
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str = None, password: str = None):
         self.username = username
+        self.password = password
+
+    def set_username(self, username: str):
+        self.username = username
+    
+    def set_password(self, password: str):
         self.password = password
     
     def generate_config(self):
@@ -35,20 +41,26 @@ class MySQLOperation:
             'host': 'localhost',
             'port': 3306,
             'user': 'root',
-            'password': 'toor'
+            'password': 'toor',
             'database': 'mysql'
         }
         return config
-    def insert_user_to_db(self):
-        config = self.generate_config()
-        with MySQLConnect(config) as cnx:
-            cursor = cnx.cursor()
-            cmd = f"CREATE USER '{self.username}'@'localhost' IDENTIFIED BY '{self.password}';"
-            try:
-                cursor.execute(cmd)
-                cnx.commit()
-                cursor.execute("flush privileges;")
-                print("sucessful")
-            except:
-                cursor.rollbac()
-                print("Unable to execute command")
+    def insert_user_to_db(self, config):
+        #config = self.generate_config()
+        try:
+            with MySQLConnect(config) as cnx:
+                cursor = cnx.cursor()
+                cmd = f"CREATE USER '{self.username}'@'localhost' IDENTIFIED BY '{self.password}';"
+                try:
+                    cursor.execute(cmd)
+                    cnx.commit()
+                    cursor.execute("flush privileges;")
+                    print("sucessful")
+                    return True
+                except:
+                    cursor.rollback()
+                    print("Unable to execute command")
+                    return False
+        except:
+            print("Unable to conenct")
+            return False
